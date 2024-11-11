8ac4556e1a8c0bf57b709ec63cb11db6c180d843
Friedrich W. H. Kossebau <kossebau@kde.org>
2024-11-11 20:21:38 +0100

    Drop support for ZXing < 2.0

--- src/core/scanimagerunner.cpp
+++ src/core/scanimagerunner.cpp
@@ -10,9 +10,6 @@
 #include "scanresult.hpp"
 #include "scanresult_p.hpp"
 // ZXing
-#include <ZXing/ZXVersion.h>
-#define ZXING_VERSION \
-       QT_VERSION_CHECK(ZXING_VERSION_MAJOR, ZXING_VERSION_MINOR, ZXING_VERSION_PATCH)
 #include <ZXing/ReadBarcode.h>
 
 namespace Kodaskanna
@@ -66,13 +63,8 @@ void ScanImageRunner::run()
 
     resultData->isSuccess = result.isValid();
     if (resultData->isSuccess) {
-#if ZXING_VERSION < QT_VERSION_CHECK(2, 0, 0)
-        resultData->text = QString::fromStdWString(result.text());
-        resultData->bytes = QByteArray(reinterpret_cast<const char *>(result.rawBytes().data()), result.rawBytes().size());        
-#else
         resultData->text = QString::fromStdString(result.text());
         resultData->bytes = QByteArray(reinterpret_cast<const char *>(result.bytes().data()), result.bytes().size());
-#endif
     }
 
     Q_EMIT scanFinished(scanResult);
